d11610657d115681bfb9fb83d012f49b74e414de
Cristiano Santos <cristianossd@gmail.com>
2017-09-19 21:36:32 -0300

    Getting periods of variations in account balance

--- web/controllers/account_controller.ex
+++ web/controllers/account_controller.ex
@@ -15,49 +15,20 @@ defmodule PhoenixDocker.AccountController do
       order_by: op.done_at
     )
 
-    periods = []
-    in_debt = false
-    debt = %{}
-    zero = D.new(0)
+    days = Enum.group_by(operations, fn(op) -> op.done_at end)
+    {periods, balance} = Enum.map_reduce(days, D.new(0), fn({date, daily_operations}, total) ->
+      balance = Enum.reduce(daily_operations, total, fn(op, acc) -> D.add(op.amount, total) end)
 
-    Enum.reduce(operations, D.new(0), fn(op, total) ->
-      balance = D.add(op.amount, total)
-
-      if D.compare(balance, zero) == D.new(-1) and D.compare(balance, total) != zero and !in_debt do
-        debt = Map.put_new(debt, :start_date, Timex.format!(op.done_at, "{0D}/{0M}/{YYYY}"))
-        debt = Map.put_new(debt, :principal, balance)
+      start_date = Timex.format! date, "{0D}/{0M}/{YYYY}"
+      in_debt = false
+      if D.compare(balance, D.new(0)) == D.new(-1) do
         in_debt = true
-      else
-        if D.compare(balance, total) != zero and in_debt do
-          day_before = Timex.shift op.done_at, days: -1
-          not_in_debt_date = Timex.format! op.done_at, "{0D}/{0M}/{YYYY}"
-          debt = Map.put_new(debt, :end_date, Timex.format!(day_before, "{0D}/{0M}/{YYYY}"))
-
-          if debt.start_date != not_in_debt_date do
-            periods ++ debt
-          end
-
-          debt = Map.put(debt, :start_date, nil)
-          debt = Map.put(debt, :end_date, nil)
-          debt = Map.put(debt, :principal, nil)
-          in_debt = false
-
-          if D.compare(balance, zero) == D.new(-1) do
-            debt = Map.put(debt, :start_date, not_in_debt_date)
-            debt = Map.put(debt, :principal, balance)
-            in_debt = true
-          end
-        end
       end
 
-      if (op == Enum.at(operations, -1)) do
-        periods ++ debt
-      end
-
-      balance
+      {%{start_date: start_date, principal: balance, in_debt: in_debt}, balance}
     end)
 
     conn
-    |> json periods
+    |> json(periods)
   end
 end
