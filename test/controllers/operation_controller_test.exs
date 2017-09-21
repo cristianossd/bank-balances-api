b3f18907e60d4a00555224f59e4e6306ac40c2f3
Cristiano Santos <cristianossd@gmail.com>
2017-09-21 08:50:45 -0300

    Removing blank line

--- test/controllers/operation_controller_test.exs
+++ test/controllers/operation_controller_test.exs
@@ -3,7 +3,6 @@ defmodule PhoenixDocker.OperationControllerTest do
   use Plug.Test
   alias PhoenixDocker.Router
 
-
   setup do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(PhoenixDocker.Repo)
     Ecto.Adapters.SQL.Sandbox.mode(PhoenixDocker.Repo, {:shared, self()})
